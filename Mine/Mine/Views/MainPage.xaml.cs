a0d4d64bb9f6db14dad8367a7649b0cefb2cf29e
Tate <tatebrasel@gmail.com>
2021-01-15 12:25:16 -0800

    Added Game Page, Updated HomeMenuItem to have Game, updated MainPage and Menu Page to include the GamePage. Change to HomeMenuItem.cs, GamePage.xaml, GamePage.xaml.cs, MainPage.xaml.cs, MenuPage.xaml.cs, and Mine.csproj

--- Mine/Mine/Views/MainPage.xaml.cs
+++ Mine/Mine/Views/MainPage.xaml.cs
@@ -30,6 +30,9 @@ namespace Mine.Views
                     case (int)MenuItemType.Items:
                         MenuPages.Add(id, new NavigationPage(new ItemsPage()));
                         break;
+                    case (int)MenuItemType.Game:
+                        MenuPages.Add(id, new NavigationPage(new GamePage()));
+                        break;
                     case (int)MenuItemType.About:
                         MenuPages.Add(id, new NavigationPage(new AboutPage()));
                         break;

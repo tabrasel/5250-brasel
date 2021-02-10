e3e8320237af20466cc8b36cf53bb3242e679948
Tate <tatebrasel@gmail.com>
2021-02-09 22:43:29 -0800

    Renamed DiceHelperUnitTest.cs to DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTests.cs
+++ UnitTests/Helpers/DiceHelperUnitTests.cs
@@ -5,7 +5,7 @@ using Mine.Helpers;
 namespace UnitTests.Helpers
 {
     [TestFixture]
-    class DiceHelperUnitTest
+    class DiceHelperUnitTests
     {
         [Test]
         public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()

20e87317dd4932dbb078f4e74ad7ea8386e74690
Tate <tatebrasel@gmail.com>
2021-02-09 23:03:11 -0800

    Added test RollDice Invalid Roll 1 Dice Should Return Zero to DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTests.cs
+++ UnitTests/Helpers/DiceHelperUnitTests.cs
@@ -66,6 +66,20 @@ namespace UnitTests.Helpers
         }
 
         [Test]
+        public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
         public void RollDice_Invalid_Roll_Forced_1_Should_Return_1()
         {
             //Arrange

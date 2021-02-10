688bd15c6907f04342974835e38499524e49ebf4
Tate <tatebrasel@gmail.com>
2021-02-09 21:31:56 -0800

    Added ItemModelTests.cs to Project

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+
+            // Reset
+
+            // Assert
+            Assert.Pass();
+        }
+    }
+}
